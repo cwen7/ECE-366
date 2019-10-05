12dbdf55a103f20a546e247715c5f47f70e764bb
arturo <agonz81@uic.edu>
2019-10-05 17:17:20 -0500

    Update MIPS-plu-sim.py
    now we have the registers working i comented out some lines so lines i use d for debuging

--- MIPS-plu-sim.py
+++ MIPS-plu-sim.py
@@ -4,6 +4,19 @@ for i in range(1025):
     g = g + 1
     mem = hex(4096*2 + i*4)
     memory.append([mem, 0, 0, 0, 0])
-    print(memory[i]) if (g == 8) else print(memory[i], end=" ")
-    if(g == 8):
-        g=0
+    # print(memory[i]) if (g == 8) else print(memory[i], end=" ")
+
+    if g == 8:
+        g = 0
+print()
+reg = [0]
+for i in range(27):         # lo == index 24 hi == index 25  PC == index 26
+    reg.append(0)
+    print(i, reg[i]) if (i < 24) else print(i, reg[i], 'reg or pc')
+
+# this will make accessing lo, hi, pc easy to remember
+
+lo = 24  # reg[lo]
+hi = 25  # reg[hi]
+pc = 26  # reg[pc]
+
