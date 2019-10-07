72a907f48456e3889713c15e811be6de6b0e9e98
arturo <agonz81@uic.edu>
2019-10-07 18:11:53 -0500

    try this for now

--- MIPS-plus-sim.py
+++ MIPS-plus-sim.py
@@ -18,7 +18,7 @@ def main():
     labelIndex = []
     labelName = []
     f = open("mc.txt", "w+")
-    h = open("Hash-MIPS-defult.asm", "r")
+    h = open("testtcase.asm", "r")
     asm = h.readlines()
     for item in range(asm.count('\n')):  # Remove all empty lines '\n'
         asm.remove('\n')
@@ -43,7 +43,7 @@ def main():
     reg = [0]
     for i in range(27):  # lo == index 24 hi == index 25  PC == index 26
         reg.append(0)
-        print('r', i, reg[i]) if (i < 24) else print('hi, lo or pc',i, reg[i])
+        ##print('r', i, reg[i]) if (i < 24) else print('hi, lo or pc',i, reg[i])
 
     # this will make accessing lo, hi, pc easy to remember
     lo = 24  # reg[lo]    reg[8] reg[pc] reg[23]
@@ -66,155 +66,30 @@ def main():
         line = line.replace(" ", "")
         line = line.replace("zero", "0")  # assembly can also use both $zero and $0
         #print(j, line)
-        if (line[0:3] == "beq"):  # BEQ
-            line = line.replace("beq", "")
-            line = line.split(",")
-            for i in range(len(labelName)):
-                if (labelName[i] == line[2]):
-                    labellocation = labelIndex[i]
-
-            ofset = labellocation - location
-            imm = format(ofset, '016b') if (ofset > 0) else format(65536 + ofset, '016b')
-            rt = format(int(line[1]), '05b')
-            rs = format(int(line[0]), '05b')
-            f.write(str('000100') + str(rs) + str(rt) + str(imm) + '\n')
-
-        if (line[0:3] == "bne"):  # BNE
-            line = line.replace("bne", "")
-            line = line.split(",")
-            for i in range(len(labelName)):
-                if (labelName[i] == line[2]):
-                    labellocation = labelIndex[i]
-
-            ofset = labellocation - location + 1
-            imm = format(ofset, '016b') if (ofset > 0) else format(65536 + ofset, '016b')
-            rt = format(int(line[1]), '05b')
-            rs = format(int(line[0]), '05b')
-            f.write(str('000101') + str(rs) + str(rt) + str(imm) + '\n')
-
-        if (line[0:4] == "sltu"):  # SLTU
-            line = line.replace("sltu", "")
-            line = line.split(",")
-            rd = format(int(line[2]), '05b')
-            rt = format(int(line[1]), '05b')
-            rs = format(int(line[0]), '05b')
-            f.write(str('000000') + str(rs) + str(rt) + str(rd) + str('00000101011') + '\n')
-
-        if (line[0:3] == "slt"):  # SLT
-            line = line.replace("slt", "")
-            line = line.split(",")
-            rd = format(int(line[2]), '05b')
-            rt = format(int(line[1]), '05b')
-            rs = format(int(line[0]), '05b')
-            f.write(str('000000') + str(rs) + str(rt) + str(rd) + str('00000101010') + '\n')
-
-        if (line[0:2] == "sw"):  # SW
-            line = line.replace("sw", "")
-            line = line.replace("(", ",")
-            line = line.replace(")", "")
-            line = line.split(",")
-            line2 = int(line[1]) if(int(line[1]) > 0) else (int(line[1])*-1)^65534 + 1
-            imm = format(line2, '016b')
-            rt = format(int(line[2]), '05b')
-            rs = format(int(line[0]), '05b')
-            f.write(str('101011') + str(rs) + str(rt) + str(imm) + '\n')
-
-
-        if (line[0:2] == "lw"):  # LW
-            line = line.replace("lw", "")
-            line = line.replace("(", ",")
-            line = line.replace(")", "")
-            line = line.split(",")
-            line2 = int(line[1]) if(int(line[1]) > 0) else (int(line[1])*-1)^65534 + 1
-            imm = format(line2, '016b')
-            rt = format(int(line[2]), '05b')
-            rs = format(int(line[0]), '05b')
-            f.write(str('100011') + str(rs) + str(rt) + str(imm) + '\n')
-
-        if (line[0:2] == "sb"):  # SB
-            line = line.replace("sb", "")
-            line = line.replace("(", ",")
-            line = line.replace(")", "")
-            line = line.split(",")
-            line2 = int(line[1]) if(int(line[1]) > 0) else (int(line[1])*-1)^65534 + 1
-            imm = format(line2, '016b')
-            rt = format(int(line[2]), '05b')
-            rs = format(int(line[0]), '05b')
-            f.write(str('101000') + str(rs) + str(rt) + str(imm) + '\n')
-
-        if (line[0:2] == "lb"):  # LB
-            line = line.replace("lb", "")
-            line = line.replace("(", ",")
-            line = line.replace(")", "")
-            line = line.split(",")
-            line2 = int(line[1]) if(int(line[1]) > 0) else (int(line[1])*-1)^65534 + 1
-            imm = format(line2, '016b')
-            rt = format(int(line[2]), '05b')
-            rs = format(int(line[0]), '05b')
-            f.write(str('100000') + str(rs) + str(rt) + str(imm) + '\n')
-
-        if (line[0:3] == "srl"):  # SRL
-            line = line.replace("srl", "")
-            line = line.split(",")
-            sh = format(int(line[2]), '05b')
-            rt = format(int(line[1]), '05b')
-            rd = format(int(line[0]), '05b')
-            f.write(str('00000000000') + str(rt) + str(rd) + str(sh) + str('000010') + '\n')
-
-        if (line[0:5] == "multu"):  # MULTU
-            line = line.replace("multu", "")
-            line = line.split(",")
-            rs = format(int(line[1]), '05b')
-            rt = format(int(line[0]), '05b')
-            f.write(str('000000') + str(rs) + str(rt) + str('0000000000011001') + '\n')
-
-        if (line[0:4] == "mult"):  # MULT
-            line = line.replace("mult", "")
-            line = line.split(",")
-            rs = format(int(line[1]), '05b')
-            rt = format(int(line[0]), '05b')
-            f.write(str('000000') + str(rs) + str(rt) + str('0000000000011000') + '\n')
-
-        if (line[0:5] == "addiu"):  # ADDIU
-            line = line.replace("addiu", "")
-            line = line.split(",")
-            imm = format(int(line[2]), '016b')
-            rs = format(int(line[1]), '05b')
-            rt = format(int(line[0]), '05b')
-            f.write(str('001001') + str(rs) + str(rt) + str(imm) + '\n')
 
         if (line[0:4] == "addi"):  # ADDI
             line = line.replace("addi", "")
             line = line.split(",")
-            imm = format(int(line[2]), '016b') if (int(line[2]) > 0) else format(65536 + int(line[2]), '016b')
+            if line[2][0:2] == "0x" or line[2][0:3] == "-0x":
+                line[2] = line[2].replace("0x", "")
+                imm = format(int(line[2], 16), '04x') if (int(line[2]) > 0) else format(int(line[2], 16), '04x')
+            else:
+                imm = format(int(line[2]), '04x')
+
+
+
+            print(int(line[2], 16))
+
+            print(imm)
+            imm = int(imm, 16) + 1
+            imm = imm + 1
+            imm = format(imm, '04x')
+            print(imm)
+
             rs = format(int(line[1]), '05b')
             rt = format(int(line[0]), '05b')
             f.write(str('001000') + str(rs) + str(rt) + str(imm) + '\n')
 
-        elif (line[0:3] == "add"):  # ADD
-            line = line.replace("add", "")
-            line = line.split(",")
-            rd = format(int(line[0]), '05b')
-            rs = format(int(line[1]), '05b')
-            rt = format(int(line[2]), '05b')
-            f.write(str('000000') + str(rs) + str(rt) + str(rd) + str('00000100000') + '\n')
-
-        elif (line[0:1] == "j"):  # JUMP
-            line = line.replace("j", "")
-            line = line.split(",")
-
-            # Since jump instruction has 2 options:
-            # 1) jump to a label
-            # 2) jump to a target (integer)
-            # We need to save the label destination and its target location
-
-            if (line[0].isdigit()):  # First,test to see if it's a label or a integer
-                f.write(str('000010') + str(format(int(line[0]), '026b')) + '\n')
-
-            else:  # Jumping to label
-                for i in range(len(labelName)):
-                    if (labelName[i] == line[0]):
-                        f.write(str('000010') + str(format(int(labelIndex[i]), '026b')) + '\n')
         if location != len(asm):
             line = asm[location]
             line = ''.join(str(e) for e in line)
